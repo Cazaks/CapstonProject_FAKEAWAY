5330fe82222fb14a30973475185d33f42ff59f37
Cazaks <calebezak@gmail.com>
2026-01-23 14:16:13 +0100

    Modifying the try now button, making it slightly smaller to 2px and giving it a gap of 3 from the pragraph

--- src/section/CallToActionSection.jsx
+++ src/section/CallToActionSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { ArrowRightIcon } from 'lucide-react';
 import SectionTitle from '../components/SectionTitle';
 
@@ -5,10 +6,10 @@ export default function CallToActionSection() {
     return (
         <section className='flex flex-col items-center justify-center py-20'>
             <SectionTitle title='Try FakeAway for Free' description='FakeAway is a secure ticketing platform that prevents counterfeit tickets and ensures smooth, contactless event entry for your events.' />
-            <a href='#' className='mt-4 flex items-center gap-2 rounded-full bg-gray-900 px-8 py-2.5 font-medium text-white transition hover:opacity-90'>
+            <Link to="/signup" className='mt-4 flex items-center gap-2 rounded-full bg-gray-900 px-8 py-2.5 font-medium text-white transition hover:opacity-90'>
                 Try now
                 <ArrowRightIcon className='size-5' />
-            </a>
+            </Link>
         </section>
     );
 }
