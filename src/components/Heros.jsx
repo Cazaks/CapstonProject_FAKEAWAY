efdf8a95e305902b496e7eadfc8f9afa8c56391c
Cazaks <calebezak@gmail.com>
2026-01-23 13:41:54 +0100

    Adding a signup link to Heros.jsx

--- src/components/Heros.jsx
+++ src/components/Heros.jsx
@@ -1,13 +1,16 @@
+import { Link} from 'react-router-dom';
 import { ArrowRightIcon } from 'lucide-react';
 
 export default function Heros() {
     return (
         <div className="flex w-full flex-wrap items-center justify-center bg-gradient-to-b from-orange-500 to-orange-600 py-2 text-center font-medium text-white">
             <p>Secure QR & NFC ticketing for modern events!</p>
-            <a href="#" className="ml-3 flex items-center gap-1 rounded-md bg-white px-3 py-1 text-xs text-orange-600 transition hover:bg-slate-200 active:scale-95">
-                Check it out
-                <ArrowRightIcon className="size-3.5" />
-            </a>
+            <Link
+  to="/signup"
+  className="hidden rounded-full bg-gray-900 px-8 py-2.5 font-medium text-white transition hover:opacity-90 md:inline-block"
+>
+  Check it out! <ArrowRightIcon className="inline-block size-4 ml-1" />
+</Link>
         </div>
     );
 }
