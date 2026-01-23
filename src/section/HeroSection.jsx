7fb27798fefde041a96116fbd839edd8d2ec40ba
Cazaks <calebezak@gmail.com>
2026-01-23 14:58:13 +0100

    Linking signUp to HeroSection.jsx to load create account once get started and get 200 credits free! buttons

--- src/section/HeroSection.jsx
+++ src/section/HeroSection.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import { ArrowRightIcon, CheckIcon, ChevronRight } from 'lucide-react';
 
 export default function HeroSection() {
@@ -12,7 +13,8 @@ export default function HeroSection() {
                 <path stroke='var(--color-gray-200)' strokeOpacity='.5' d='M-15.227 573.66H1439.7M-15.227 164.029H1439.7' />
                 <circle cx='782.595' cy='411.166' r='308.334' stroke='var(--color-gray-200)' strokeOpacity='.5' />
             </svg>
-            <a href='#' className='mt-32 flex items-center gap-2 rounded-full border border-gray-200 bg-gray-50 px-3 py-1 text-sm font-medium transition hover:bg-gray-100/80'>
+            <Link 
+            to="/signup" className='mt-32 flex items-center gap-2 rounded-full border border-gray-200 bg-gray-50 px-3 py-1 text-sm font-medium transition hover:bg-gray-100/80'>
                 <svg width='16' height='16' viewBox='0 0 16 16' fill='none' xmlns='http://www.w3.org/2000/svg'>
                     <g clipPath='url(#a)' stroke='#1D293D' strokeWidth='1.5' strokeLinecap='round' strokeLinejoin='round'>
                         <path d='M6.626 10.333a1.33 1.33 0 0 0-.958-.958l-4.09-1.054a.333.333 0 0 1 0-.642l4.09-1.055a1.33 1.33 0 0 0 .958-.957l1.055-4.09a.333.333 0 0 1 .642 0l1.054 4.09a1.33 1.33 0 0 0 .958.958l4.09 1.054a.333.333 0 0 1 0 .642l-4.09 1.054a1.33 1.33 0 0 0-.958.958l-1.055 4.09a.334.334 0 0 1-.642 0zM13.336 2v2.667m1.331-1.334H12m-9.336 8v1.333M3.333 12H2' />
@@ -27,15 +29,15 @@ export default function HeroSection() {
                     <span>Sign up now and get 200 credits free!</span>
                     <ChevronRight className='size-4' />
                 </p>
-            </a>
+            </Link>
 
             <h1 className='mt-4 max-w-3xl scale-105 bg-gradient-to-r from-black to-[#748298] bg-clip-text text-center text-4xl/12 font-bold text-transparent md:scale-100 md:text-6xl/20'>Fraud-proof tickets? QR & NFC tickets is the answer.</h1>
             <p className='mt-2 max-w-xl text-center text-base/7 text-gray-700'>We’ve got you covered against ticket fraud, giving event organizers peace of mind with QR and NFC tickets designed to prevent copying, reuse, and unauthorized access.</p>
 
-            <a href='/' className='mt-8 flex items-center gap-2 rounded-full bg-gray-900 px-8 py-2.5 font-medium text-white transition hover:opacity-90'>
+            <Link to="/signup" className='mt-8 flex items-center gap-2 rounded-full bg-gray-900 px-8 py-2.5 font-medium text-white transition hover:opacity-90'>
                 <span>get started</span>
                 <ArrowRightIcon className='size-5' />
-            </a>
+            </Link>
 
             <div className='mt-12 flex flex-wrap items-center justify-center gap-4 text-gray-500 md:gap-14'>
                 {specialFeatures.map((feature, index) => (
