2af916f2652bb1fe40c460960714116ecedd28d4
Cazaks <calebezak@gmail.com>
2026-01-26 18:23:43 +0100

    Adding background image to dashboad

--- src/pages/dashboard/Dashboardlayout.jsx
+++ src/pages/dashboard/Dashboardlayout.jsx
@@ -1,11 +1,39 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Outlet, useNavigate } from "react-router-dom";
 
-export default function DashboardLayout() {
+export default function DashBoardLayout() {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem("isLoggedIn");
+    navigate("/login");
+  };
+
   return (
-    <div className="flex h-screen bg-gray-100">
+    <div className="relative h-screen w-full flex">
 
-      <aside className="w-64 bg-white border-r">
-        <div className="p-6 text-xl font-bold">FAKEAWAY</div>
+
+      <div
+        className="absolute inset-0 bg-cover bg-center"
+        style={{
+          backgroundImage: "url('/assets/first_bgImage.jpg')",
+        }}
+      />
+
+
+      <div className="absolute inset-0 bg-black/40" />
+
+      <aside className="relative z-10 w-64 bg-gray-100 border-r">
+        <div className="flex items-center gap-1">
+        <img
+          src="/assets/logo.svg"
+          alt="logo"
+          className="h-12 w-10 shrink-0 md:h-14 md:w-10"
+        />
+
+        <span className="text-base md:text-4xl font-extrabold text-blue-469 leading-none">
+          FAKEAWAY
+        </span>
+        </div>
 
         <nav className="flex flex-col gap-2 px-4">
           <Link to="/dashboard" className="p-2 rounded hover:bg-gray-100">
@@ -20,11 +48,18 @@ export default function DashboardLayout() {
           <Link to="/dashboard/profile" className="p-2 rounded hover:bg-gray-100">
             Profile
           </Link>
+
+          <button
+            onClick={handleLogout}
+            className="mt-6 p-2 text-left rounded hover:bg-red-100 text-red-600"
+          >
+            Logout
+          </button>
         </nav>
       </aside>
 
-      
-      <main className="flex-1 p-6 overflow-y-auto">
+
+      <main className="relative z-10 flex-1 p-6 overflow-y-auto">
         <Outlet />
       </main>
     </div>
