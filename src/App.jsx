3162124aa36cb142e21f50b6e760fcb5876c8aa6
Cazaks <calebezak@gmail.com>
2026-02-02 02:50:48 +0100

    Refactoring App.jsx to accomodate the create event files and flolders

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "./pages/LandingPage";
 import SignUp from "./pages/SignUp";
+import Login from "./pages/Login";
+import ForgotPassword from "./pages/ForgotPassword";
+import DashBoardLayout from "./pages/dashboard/DashBoardLayout";
+// import DashboardHome from "./pages/dashboard/DashboardHome";
+import CreateEvents from "./pages/eventlayout/CreateEvents";
+import EventPages from "./pages/eventlayout/EventsPageLayout";
 
 function App() {
   return (
@@ -8,6 +14,16 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/forgotpassword" element={<ForgotPassword />} />
+
+        <Route path="/dashboard" element={<DashBoardLayout />}>
+          {/* <Route index element={<DashboardHome />} /> */}
+
+          <Route path="create-events" element={<EventPages />}>
+            <Route index element={<CreateEvents />} />
+          </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
